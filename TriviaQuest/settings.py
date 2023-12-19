34b9a98e6aa49a055de6ec995338f33f4ec21da5
zlcosio21 <sebas60621@gmail.com>
2023-12-18 22:29:04 -0500

    Usando BBDD Mysql e implementacion de imagenes

--- TriviaQuest/settings.py
+++ TriviaQuest/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'TriviaQuest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trivia_quest',
+        'USER': 'root',
+        'PASSWORD': 'minato21',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

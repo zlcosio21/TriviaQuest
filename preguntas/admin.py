d2c81b457b32cca2e55f662bd0572f298d5786d3
zlcosio21 <sebas60621@gmail.com>
2023-12-19 22:56:55 -0500

    Registrando los modelos en el admin

--- preguntas/admin.py
+++ preguntas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from preguntas.models import CategoriaQuiz, Quiz
 
 # Register your models here.
+class CategoriaQuizAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class QuizAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(CategoriaQuiz, CategoriaQuizAdmin)
+admin.site.register(Quiz, QuizAdmin)

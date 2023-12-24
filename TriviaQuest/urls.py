ae641bef9c38255a38ac485c20144464be389b7a
zlcosio21 <sebas60621@gmail.com>
2023-12-24 14:08:22 -0500

    Corrigiendo urls de la app quiz

--- TriviaQuest/urls.py
+++ TriviaQuest/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("TriviaQuestApp.urls")),
-    path('preguntas/', include("preguntas.urls")),
+    path('quiz/', include("quiz.urls")),
 ]

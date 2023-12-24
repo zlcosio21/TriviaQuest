60c0e2f7fb3765f29e3824b216f059c529fd99a4
zlcosio21 <sebas60621@gmail.com>
2023-12-24 16:38:35 -0500

    Tomando 4 registros de categorias y 6 de quizes

--- TriviaQuestApp/views.py
+++ TriviaQuestApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from quiz.models import CategoriaQuiz, Quiz
 
 # Create your views here.
 def inicio(request):
+    categorias = CategoriaQuiz.objects.all()[:4]
+    quizes = Quiz.objects.all()[:6]
 
-    return render(request, "TriviaQuestApp/inicio.html")
+    return render(request, "TriviaQuestApp/inicio.html", {"quizes":quizes, "categorias":categorias})

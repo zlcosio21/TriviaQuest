a85450843faed9d5a502becaed43cd11124bc152
zlcosio21 <sebas60621@gmail.com>
2023-12-27 19:48:51 -0500

    Urls de las vistas creadas

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,4 +5,5 @@ from quiz import views
 urlpatterns = [
     path('creacion_quiz/', views.creacion_quiz, name="creacion_quiz"),
     path('eleccion_quizes/', views.eleccion_quizes, name="eleccion_quizes"),
+    path('juego_quiz/', views.juego_quiz, name="juego_quiz"),
 ]

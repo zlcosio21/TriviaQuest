df8a7614444f00ab8715d4b525fd61da2c0b750a
zlcosio21 <sebas60621@gmail.com>
2023-12-27 19:50:30 -0500

    Js para la plantilla de juego, creando un contador

--- TriviaQuestApp/static/assets/js/main.js
+++ TriviaQuestApp/static/assets/js/main.js
@@ -8,3 +8,40 @@ document.addEventListener('DOMContentLoaded', function () {
     this.style.width = Math.max(width, initialSize) + 'ch';
   });
 });
+
+const playAudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
+
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text"),
+  selectTime = document.getElementById("time");
+
+let time = parseInt(selectTime.dataset.valor);
+let intervalId;
+
+const startQuestion = () => {
+  progress(0);
+  intervalId = setInterval(() => {
+    progress(time);
+    time--;
+
+    if (time < 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+};
+
+const progress = (value) => {
+  const percentage = (value / parseInt(selectTime.dataset.valor)) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}s`;
+};
+
+startQuestion();
+
+selectTime.addEventListener("change", () => {
+  time = parseInt(selectTime.dataset.valor);
+  startQuestion();
+});

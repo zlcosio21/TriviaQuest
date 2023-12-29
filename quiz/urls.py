cefae119abd521967c6e5ff717fc236f5987037b
zlcosio21 <sebas60621@gmail.com>
2023-12-28 22:25:00 -0500

    urls de las vistas

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,5 +5,6 @@ from quiz import views
 urlpatterns = [
     path('creacion_quiz/', views.creacion_quiz, name="creacion_quiz"),
     path('eleccion_quizes/', views.eleccion_quizes, name="eleccion_quizes"),
-    path('juego_quiz/', views.juego_quiz, name="juego_quiz"),
+    path('juego_quiz/<int:respuestas_correctas>/<int:pregunta_actual>/<int:num_preguntas>/<str:categoria>/<int:tiempo>/', views.juego_quiz, name="juego_quiz"),
+    path('comprobar_respuesta/<str:pregunta>/<int:respuestas_correctas>/<int:pregunta_actual>/<int:num_preguntas>/<str:categoria>/<int:tiempo>/', views.comprobar_respuesta, name="comprobar_respuesta"),
 ]

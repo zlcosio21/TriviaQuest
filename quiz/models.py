e2fd3c1a8cf1a344a6540aafd6d2eb684caa9152
zlcosio21 <sebas60621@gmail.com>
2024-01-04 16:26:27 -0500

    Preguntas unicas, categorias unicas, y null siempre false

--- quiz/models.py
+++ quiz/models.py
@@ -5,7 +5,7 @@ from TriviaQuest.base_models import Models
 # Create your models here.
 
 class CategoriaQuiz(Models):
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30, unique=True, null=False)
 
     def __str__(self):
         return self.nombre
@@ -13,12 +13,12 @@ class CategoriaQuiz(Models):
 class Quiz(Models):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     categoria = models.ForeignKey(CategoriaQuiz, on_delete=models.CASCADE)
-    pregunta = models.CharField(max_length=100)
-    primera_opcion = models.CharField(max_length=90)
-    segunda_opcion = models.CharField(max_length=90)
-    tercera_opcion = models.CharField(max_length=90)
-    cuarta_opcion = models.CharField(max_length=90)
-    opcion_correcta = models.CharField(max_length=90)
+    pregunta = models.CharField(max_length=100, unique=100, null=False)
+    primera_opcion = models.CharField(max_length=90, null=False)
+    segunda_opcion = models.CharField(max_length=90, null=False)
+    tercera_opcion = models.CharField(max_length=90, null=False)
+    cuarta_opcion = models.CharField(max_length=90, null=False)
+    opcion_correcta = models.CharField(max_length=90, null=False)
 
     def __str__(self):
         return f"Usuario {self.usuario.username} - Categoria {self.categoria.nombre} - Pregunta {self.pregunta} - Correcta {self.opcion_correcta}"

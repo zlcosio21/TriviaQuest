16edd5d4d9dcf2f89ac75b21307fc5d6dbc3954f
zlcosio21 <sebas60621@gmail.com>
2024-04-21 00:03:12 -0500

    Eliminando la app perfil

--- perfil/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from perfil import views
-
-urlpatterns = [
-    path('', views.perfil, name="perfil"),
-    path('editar_perfil/', views.editar_perfil, name="editar_perfil")
-]

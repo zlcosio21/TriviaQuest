2aff87639ee1a59c377607c2d58f40b3d16e7cf8
zlcosio21 <sebas60621@gmail.com>
2024-06-28 22:39:28 -0500

    Actualizando la vista inicio

--- TriviaQuestApp/views.py
+++ TriviaQuestApp/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from quiz.models import CategoriaQuiz, Quiz
-from random import sample
+from django.shortcuts import render
 
 def inicio(request):
-    categorias = sample(list(CategoriaQuiz.objects.all()), min(4, CategoriaQuiz.objects.count()))
-    quizes = sample(list(Quiz.objects.all()), min(6, Quiz.objects.count()))
+    quizes = Quiz.random_quizes()
+    categorias = CategoriaQuiz.random_categories()
 
     return render(request, "TriviaQuestApp/inicio.html", {"quizes":quizes, "categorias":categorias})
